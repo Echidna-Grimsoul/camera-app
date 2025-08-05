e05c4e500ae532c0a5bda9bc4e8a7ec72de16187
Echidna-Grimsoul <lernaeanlabs@gmail.com>
2025-08-05 17:35:36 -0500

    Scaffold Express app

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server on ${PORT}`));
